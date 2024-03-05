e36df0c444ed49fa86a06de3bbc321f459e0d1d2
Clayton Rafael <clayton.rssouza@gmail.com>
2024-03-05 08:26:04 -0300

    feat: implement aboutPage and menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import Menu from "@/components/components/menu";
 
 export const metadata: Metadata = {
   title: "Curso de NextJS",
@@ -14,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Menu />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Curso de NextJS | Sobre",
+    description: "Criado por Clayton Rafael",
+  };
+  export default function SobrePage() {
+    return (
+      <main>
+        <h1>Sobre</h1>
+      </main>
+    );
+  }
+  
